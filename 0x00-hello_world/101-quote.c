5589e41c8b4c2f317286262fabf4e096c4ad41de
Teddy-Belete <twy2007q@gmail.com>
2023-06-18 08:02:46 -0700

    101-quote. edited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar,2015-10-19",
+ * main - prints "and that piece of arts is useful" - Dora Korpar, 2015-10-19",
  * follwed by a newline, to standard error.
- * Return 1 (Success)
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", stdout);
 	return (1);
 }
