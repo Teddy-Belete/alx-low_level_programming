81b6b227e6df09f36f4a9b2415a45afbfcf8f637
Teddy-Belete <twy2007q@gmail.com>
2023-06-19 07:31:51 -0700

    0-positive_or_negative.c edited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Disciption: 'postive or nagative'
+ * Disciption: 'positive or nagative'
  * Return: Always 0
  */
 int main(void)
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is postive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is nagative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
