1ad399100ecb523b1b020f21a182c47d956d2411
Teddy-Belete <twy2007q@gmail.com>
2023-06-21 11:10:52 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make alphabet
+ * Description: 'print alphabet'
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
